c1fadfed90745d373b04de06d0314182f9116188
Vadrix <166731295+MrVadrix@users.noreply.github.com>
2024-05-04 04:38:33 -0700

    bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import discord
+import discord, random
 from discord.ext import commands
 
 intents = discord.Intents.default()
@@ -11,6 +11,20 @@ async def on_ready():
     print(f'We have logged in as {bot.user}')
 
 @bot.command()
+async def random_emoji(ctx):
+    emoji_list = ["😀","😁","😂","🤣","😎","🥱","🥳","🤓"]
+    emoji_chosen = random.choice(emoji_list)
+    await ctx.send(f"{emoji_chosen}")
+
+@bot.command()
+async def coin_toss(ctx):
+    coin_side = random.randint(1,2)
+    if coin_side == 1:
+        await ctx.send(f"Монетка упала на орла!")
+    elif coin_side == 2:
+        await ctx.send(f"Монетка упала на режку!")
+
+@bot.command()
 async def hello(ctx):
     await ctx.send(f'Привет! Я бот {bot.user}!')
 
@@ -20,7 +34,6 @@ async def heh(ctx, count_heh = 5):
 
 @bot.command()
 async def repeat(ctx, times: int, content='repeating...'):
-    """Repeats a message multiple times."""
     for i in range(times):
         await ctx.send(content)
 
