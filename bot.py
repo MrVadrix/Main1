a89f14de164097d1722722a26dbcda6f8063b645
Vadrix <166731295+MrVadrix@users.noreply.github.com>
2024-05-18 23:29:08 -0700

    bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,4 @@
-import discord
-import random
-import os
-import requests
+import discord, random, os, requests
 from discord.ext import commands
 
 intents = discord.Intents.default()
@@ -12,6 +9,20 @@ bot = commands.Bot(command_prefix='$', intents=intents)
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def command_list(ctx):
+    await ctx.send(f"Список всех команд:")
+    await ctx.send("**$random_emoji**  ->  рандомный емодзи")
+    await ctx.send("**$coin_toss**  ->  кидает монетку и говорит на какой стороне упало")
+    await ctx.send("**$hello**  ->  говорит привет тебе :)")
+    await ctx.send("**$heh**  ->  напиши heh и число, он столько повторит heh")
+    await ctx.send("**$repeat**  ->  впиши слово после комнады и ещё число сколько повторить")
+    await ctx.send("**$meme**  ->  скидывает рандомный мем")
+    await ctx.send("**$dog**  ->  рандомная собака")
+    await ctx.send("**$duck**  ->  рандомная утка")
+    await ctx.send("**$animal**  ->  рандомное животное")
+    await ctx.send("**$eko_list**  ->  список команд с темой 'екология' ")
 
 @bot.command()
 async def random_emoji(ctx):
@@ -49,16 +60,6 @@ async def meme(ctx):
     await ctx.send(file=picture)
 
     '''API'''
-@bot.command()
-async def animal(ctx):
-    animal_choice = random.randint(1,2)
-    if animal_choice == 1:
-        image_url = get_dog_image_url()
-        await ctx.send(image_url)
-    elif animal_choice == 2:
-        image_url = get_duck_image_url()
-        await ctx.send(image_url)
-
 def get_dog_image_url():
     url = 'https://random.dog/woof.json'
     res = requests.get(url)
@@ -80,6 +81,51 @@ async def dog(ctx):
 async def duck(ctx):
     image_url = get_duck_image_url()
     await ctx.send(image_url)
+
+@bot.command()
+async def animal(ctx):
+    animal_choice = random.randint(1,2)
+    if animal_choice == 1:
+        image_url = get_dog_image_url()
+        await ctx.send(image_url)
+    elif animal_choice == 2:
+        image_url = get_duck_image_url()
+        await ctx.send(image_url)
+
+    '''eko'''
+eko_fact1 = "Глобальное потепление вызвано деятельностью человека, выбрасывающей парниковые газы, что приводит к устойчивому повышению средней температуры на Земле."
+eko_fact2 = "Глобального потепления последствия включают повышение уровня моря, более экстремальные погодные явления, такие как волны тепла и штормы, а также нарушения экосистем и сельского хозяйства."
+eko_fact3 = "Решение проблемы глобального потепления требует сокращения выбросов парниковых газов посредством устойчивых методов и перехода на возобновляемые источники энергии."
+
+ice_glacier_fact1 = "Таяние ледников влияет на уровень мировых океанов, увеличивая его и угрожая прибрежным областям и экосистемам."
+ice_glacier_fact2 = "Этот процесс ускоряется из-за выбросов парниковых газов, таких как углекислый газ, в атмосферу из-за человеческой деятельности."
+ice_glacier_fact3 = "Таяние ледников происходит из-за глобального потепления, которое приводит к повышению средней температуры на Земле."
+
+@bot.command()
+async def eko_list(ctx):
+    await ctx.send("Список команд, содержащих экологические аспекты:")
+    await ctx.send("**$global_warming_facts**   ->   факты о глобальном потеплении")
+    await ctx.send("**$ice_glaciers_melting**    ->   информация о таянии ледников")
+
+@bot.command()
+async def global_warming_facts(ctx):
+    global_warming_fact_chosen = random.randint(1,3)
+    if global_warming_fact_chosen == 1:
+        await ctx.send(f"Вот вам факт о глобальном потеплении:\n{eko_fact1}")
+    if global_warming_fact_chosen == 2:
+        await ctx.send(f"Вот вам факт о глобальном потеплении:\n{eko_fact2}")
+    if global_warming_fact_chosen == 3:
+        await ctx.send(f"Вот вам факт о глобальном потеплении:\n{eko_fact3}")
+
+@bot.command()
+async def ice_glaciers_melting(ctx):
+    ice_glaciers_fact_chosen = random.randint(1,3)
+    if ice_glaciers_fact_chosen == 1:
+        await ctx.send(f"Вот вам факт о ледниках:\n{ice_glacier_fact1}")
+    if ice_glaciers_fact_chosen == 2:
+        await ctx.send(f"Вот вам факт о ледниках:\n{ice_glacier_fact2}")
+    if ice_glaciers_fact_chosen == 3:
+        await ctx.send(f"Вот вам факт о ледниках:\n{ice_glacier_fact3}")
 
 #TOKEN
 bot.run()
